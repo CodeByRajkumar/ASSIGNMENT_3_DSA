05c001b3e8b8d00b8a807a531a3ee5572660e94f
Raj Kumar <131976083+CodeByRajkumar@users.noreply.github.com>
2023-07-18 12:34:30 +0530

    Update Q2.java

--- ASSIGNMENT_3_CW/Q2.java
+++ ASSIGNMENT_3_CW/Q2.java
@@ -1,16 +1,13 @@
 import java.util.*;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class Q2 {
 	public static void main(String[] args) {
-		
 		String s[] = new String[4];
 		Scanner sc =  new Scanner(System.in);
+		
 		try {
 			System.out.println("Enter the four colous");
 			for (int i=0;i<s.length;i++) {
 				s[i]=sc.next();
-				
 			}
 			System.out.println("Convert string to integer");
 			Integer.parseInt(s[0]);
@@ -24,7 +21,6 @@ public class Q2 {
 			System.out.println("Enter one more colour");
 			s[4]=sc.next();
 			throw new ArrayIndexOutOfBoundsException("Index "+ 5 +" out of bounds for length 4()");
-		
 		}
 		catch(ArrayIndexOutOfBoundsException f){
 			System.out.println(f.toString());
@@ -34,7 +30,6 @@ public class Q2 {
 			System.out.println(s[i]);
 		}
 	}
-
 }
 /*OUTPUT:
 Enter the four colous
